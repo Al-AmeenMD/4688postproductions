3d5aeeafb8053fdbf677fe2b209682eda7d10901
Al-Amin Muhammad <alaminmuhammad611@gmail.com>
2025-04-04 17:35:58 +0100

    Update repository name configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/4688productions/',
+  base: '/4688postproductions/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
